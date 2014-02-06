9eecbb2f315575e7830c1783a6c7e71ae416f958
Nick Walter <nickspage@gmail.com>
2014-02-06 14:03:20 -0700

    Initial Commit

---
+++ TabBarContainerNavProblem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TabBarContainerNavProblem
+//
+//  Created by Nick Walter on 2/6/14.
+//  Copyright (c) 2014 Nick Walter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TabBarContainerNavProblem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TabBarContainerNavProblem
+//
+//  Created by Nick Walter on 2/6/14.
+//  Copyright (c) 2014 Nick Walter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
